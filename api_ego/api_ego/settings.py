38f1438d6b9badb7b9b175fad70e79df56f1f53e
Franco-Micheletti <79060860+Franco-Micheletti@users.noreply.github.com>
2023-06-16 13:03:58 -0300

    updated env

--- api_ego/api_ego/settings.py
+++ api_ego/api_ego/settings.py
@@ -11,17 +11,18 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv.main import load_dotenv
+load_dotenv('.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$pdah$mg-l*ljm)$^5_5&l9ipn5wm54vnpn+@i(+eap1ue*ubs'
-
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
